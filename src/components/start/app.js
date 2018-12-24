0983ceabf3ea05e48cc4912a27ac39e9204cfa9f
bigshaqmnh <kmikecoder@gmail.com>
2018-12-24 22:39:50 +0300

    first commit

---
+++ src/components/start/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../resources/img/react_logo.png';
+
+import './app.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My new React App!</h1>
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  module: {    
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+          "eslint-loader"
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
